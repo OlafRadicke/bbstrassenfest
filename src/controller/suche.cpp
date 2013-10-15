db218b44d989534c8e7617c268f91a50a2b02232
Olaf Radicke <briefkasten@olaf-radicke.de>
2013-10-15 20:44:29 +0200

    experiment mit namespace lösung.

--- src/controller/suche.cpp
+++ src/controller/suche.cpp
@@ -6,6 +6,7 @@
 #include <cxxtools/log.h>
 #include <strassenfestresult.h>
 #include <strassenfestmanager.h>
+#include <SessionShareds.h>
 
 log_define("suche.controller")
 
@@ -24,16 +25,18 @@ namespace
     TNT_APPLICATION_SHARED_VAR(std::vector<std::string>, bezirke, ());
     TNT_REQUEST_SHARED_VAR(StrassenfestResult, strassenfestResult, ());
 
-    TNT_SESSION_SHARED_VAR(std::string, q, ());
-    TNT_SESSION_SHARED_VAR(std::string, bezirk, ());
-    TNT_SESSION_SHARED_VAR(std::string, von_from, ());
-    TNT_SESSION_SHARED_VAR(std::string, von_to, ());
-    TNT_SESSION_SHARED_VAR(cxxtools::Date, from, ());
-    TNT_SESSION_SHARED_VAR(cxxtools::Date, to, ());
-    TNT_SESSION_SHARED_VAR(unsigned, pageNo, (1));
-    TNT_SESSION_SHARED_VAR(unsigned, itemsPerPage, (20));
+    TNT_SESSION_SHARED_VAR(SessionShareds, sessionShareds, ());
 
-    log_debug("sucheController; q=" << qparam.getUrl());
+//     TNT_SESSION_SHARED_VAR(std::string, q, ());
+//     TNT_SESSION_SHARED_VAR(std::string, bezirk, ());
+//     TNT_SESSION_SHARED_VAR(std::string, von_from, ());
+//     TNT_SESSION_SHARED_VAR(std::string, von_to, ());
+//     TNT_SESSION_SHARED_VAR(cxxtools::Date, from, ());
+//     TNT_SESSION_SHARED_VAR(cxxtools::Date, to, ());
+//     TNT_SESSION_SHARED_VAR(unsigned, pageNo, (1));
+//     TNT_SESSION_SHARED_VAR(unsigned, itemsPerPage, (20));
+
+    log_debug("sucheController; sessionShareds.q=" << qparam.getUrl());
 
     StrassenfestManager manager;
 
@@ -46,30 +49,30 @@ namespace
     {
       log_debug("suchen");
 
-      q        = qparam.arg<std::string>("q");
-      bezirk   = qparam.arg<std::string>("bezirk");
+      sessionShareds.q        = qparam.arg<std::string>("q");
+      sessionShareds.bezirk   = qparam.arg<std::string>("bezirk");
 
-      von_from = qparam.arg<std::string>("von_from");
-      von_to   = qparam.arg<std::string>("von_to");
+      sessionShareds.von_from = qparam.arg<std::string>("von_from");
+      sessionShareds.von_to   = qparam.arg<std::string>("von_to");
 
-      if (!von_from.empty())
-        from = cxxtools::Date(von_from, "%d.%m.%Y");
+      if (!sessionShareds.von_from.empty())
+        sessionShareds.from = cxxtools::Date(sessionShareds.von_from, "%d.%m.%Y");
 
-      if (!von_to.empty())
-        to = cxxtools::Date(von_to, "%d.%m.%Y");
+      if (!sessionShareds.von_to.empty())
+        sessionShareds.to = cxxtools::Date(sessionShareds.von_to, "%d.%m.%Y");
 
-      pageNo = 1;
-      strassenfestResult = manager.search(q, bezirk, from, to, cxxtools::Date(), itemsPerPage, pageNo);
+      sessionShareds.pageNo = 1;
+      strassenfestResult = manager.search(sessionShareds.q, sessionShareds.bezirk, sessionShareds.from, sessionShareds.to, cxxtools::Date(), sessionShareds.itemsPerPage, sessionShareds.pageNo);
     }
     else if (qparam.arg<bool>("previousPage"))
     {
-      --pageNo;
-      strassenfestResult = manager.search(q, bezirk, from, to, cxxtools::Date(), itemsPerPage, pageNo);
+      --sessionShareds.pageNo;
+      strassenfestResult = manager.search(sessionShareds.q, sessionShareds.bezirk, sessionShareds.from, sessionShareds.to, cxxtools::Date(), sessionShareds.itemsPerPage, sessionShareds.pageNo);
     }
     else if (qparam.arg<bool>("nextPage"))
     {
-      ++pageNo;
-      strassenfestResult = manager.search(q, bezirk, from, to, cxxtools::Date(), itemsPerPage, pageNo);
+      ++sessionShareds.pageNo;
+      strassenfestResult = manager.search(sessionShareds.q, sessionShareds.bezirk, sessionShareds.from, sessionShareds.to, cxxtools::Date(), sessionShareds.itemsPerPage, sessionShareds.pageNo);
     }
 
     // Return DECLINED to tell tntnet to continue processing with next mapping.
